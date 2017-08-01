52e71b67086a9810621a2208d17f7cde8ab58fc9
shota-saito <s.saito@timers-inc.com>
2017-08-01 15:33:36 +0900

    Delete unuse resources

--- app/src/main/java/jp/shts/android/library/sample/clockanimationview/MainApplication.java
+++ app/src/main/java/jp/shts/android/library/sample/clockanimationview/MainApplication.java
@@ -2,12 +2,9 @@ package jp.shts.android.library.sample.clockanimationview;
 
 import android.app.Application;
 
-import com.jakewharton.threetenabp.AndroidThreeTen;
-
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        AndroidThreeTen.init(this);
     }
 }
